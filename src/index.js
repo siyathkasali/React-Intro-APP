44de227d3dc8a39e734522781909d9386b71b765
Siyath Kasali <siyaths19@gmail.com>
2017-08-07 23:53:43 +0530

    Created and tested component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var React = require('react');
+var ReactDom = require('react-dom');
+var createClass = require('create-react-class');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+var Component = createClass({
+    render:function(){
+        return(
+        <h1>Hello World</h1>
+    );
+    }
+});
+
+ReactDom.render(
+<Component />,
+document.getElementById('root')
+);
